84c597bf0001915c415d1796c39e5793545b99ba
Mostafa <ashrafimostafa@gmail.com>
2024-09-04 00:52:10 +0330

    Update MainAtivity ui

--- app/src/main/java/ir/mostafa/ping/ui/MainActivity.kt
+++ app/src/main/java/ir/mostafa/ping/ui/MainActivity.kt
@@ -1,21 +1,30 @@
 package ir.mostafa.ping.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import ir.mostafa.ping.R
+import ir.mostafa.ping.databinding.ActivityMainBinding
+import ir.mostafa.ping.utility.clickWithDebounce
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+
+            mainAnimation.apply {
+
+            }
+
+            mainAnimation.clickWithDebounce {
+                mainAnimation.playAnimation()
+            }
+
         }
+
     }
 }
